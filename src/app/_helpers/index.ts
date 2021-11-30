b313f085a56a9e841373e3c3fc732600277e3f30
DexT3R5845 <valentin2747@gmail.com>
2021-12-01 00:39:50 +0200

    Add guard for auth forms.

--- src/app/_helpers/index.ts
+++ src/app/_helpers/index.ts
@@ -1,1 +1,6 @@
 export * from './jwt.inerceptor'
+export * from './auth.guard'
+export * from './authForms.guard'
+export * from './cookies.storage'
+export * from './error.interceptor'
+export * from './must-match.validator'
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './_helpers/auth.guard';
+import { AuthFormsGuard, AuthGuard } from './_helpers';
 import { Role } from './_models/role';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
@@ -9,7 +9,7 @@ const profileModule = () => import('./profile/profile.module').then(x => x.Profi
 
 const routes: Routes = [
   { path: '', redirectTo: '/account/signin', pathMatch: 'full' },
-  { path: 'account', loadChildren: accountModule },
+  { path: 'account', loadChildren: accountModule, canActivate: [AuthFormsGuard] },
   { path: 'admin', loadChildren: adminModule, canActivate: [AuthGuard], data: { roles: [Role.Admin, Role.User] } },
   { path: 'profile', loadChildren: profileModule, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/account/signin', pathMatch: 'full' }
