bd4f7d00759f8346756ec255640e7f75d26e217a
vladsurhai <vlasurgay@gmail.com>
2021-12-15 23:49:52 +0200

    try to set up heroku 10

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   serverUrl: 'https://bakensweets-server.herokuapp.com/api',
   tokenName: 'bakensweetsToken',
   siteKey: '6Lf8XUodAAAAAN_wbdoV37oas0iRYiRSBmSdCKJG',
