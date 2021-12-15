895a071863fce62d54613229d4ea6cc03ccee990
vladsurhai <vlasurgay@gmail.com>
2021-12-15 21:31:15 +0200

    try to set up heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:8080/api',
+  serverUrl: 'https://bakensweets-server.herokuapp.com/api',
   tokenName: 'bakensweetsToken',
   siteKey: '6Lf8XUodAAAAAN_wbdoV37oas0iRYiRSBmSdCKJG',
   pageSize: 12
