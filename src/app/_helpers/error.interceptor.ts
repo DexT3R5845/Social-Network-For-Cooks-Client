a8e06913e7c25ab99b6872cdc4ea9fb1d246a673
DexT3R5845 <valentin2747@gmail.com>
2021-12-17 20:06:33 +0200

    Add interceptor for 403

---
+++ src/app/_helpers/error.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services';
+
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(error => {
+            if ([403].includes(error.status) && this.authService.accountValue) {
+                this.authService.logout();
+            }
+            return throwError(() => error);
+        }))
+    }
+}
