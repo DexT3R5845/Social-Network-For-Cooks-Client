4ae30879594fb36fc60c97a4f4d78c8989da268c
Yevhenii Malakhov <malakhov747@gmail.com>
2021-12-19 13:09:21 +0200

    Did some refactoring.

--- src/app/_services/profile.service.ts
+++ src/app/_services/profile.service.ts
@@ -1,33 +1,35 @@
 import {HttpClient, HttpParams} from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
-import { Profile } from "../_models";
+import {Injectable} from "@angular/core";
+import {environment} from "src/environments/environment";
+import {Profile} from "../_models";
+import {Observable} from "rxjs";
 
 const baseUrl = `${environment.serverUrl}/user`;
 
 @Injectable({
-    providedIn: 'root'
-  })
+  providedIn: 'root'
+})
 export class ProfileService {
 
   constructor(
-      private http: HttpClient,
-  ){}
+    private http: HttpClient,
+  ) {
+  }
 
-  saveChanges(profile: Profile) {
-    return this.http.put(
+  saveChanges(profile: Profile): Observable<Profile> {
+    return this.http.put<Profile>(
       `${baseUrl}/profile`, profile
     )
   }
 
-  getProfileData() {
-      return this.http.get<Profile>(`${baseUrl}/profile`);
+  getProfileData(): Observable<Profile> {
+    return this.http.get<Profile>(`${baseUrl}/profile`);
   }
 
-  changePassword(oldPassword:string, newPassword:string){
+  changePassword(oldPassword: string, newPassword: string): Observable<Object> {
     let reqParams = new HttpParams();
     reqParams = reqParams.append('oldPassword', oldPassword);
     reqParams = reqParams.append('newPassword', newPassword);
-    return this.http.put(`${baseUrl}/profile/changePassword`,{},{params:reqParams})
+    return this.http.put(`${baseUrl}/profile/changePassword`, {}, {params: reqParams})
   }
 }
