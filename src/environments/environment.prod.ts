3769cf52fa3037a955b23f0be8d886e1033a81d4
DexT3R5845 <valentin2747@gmail.com>
2021-12-21 13:45:30 +0200

    Fix key recaptcha

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   serverUrl: 'https://bakensweets-server.herokuapp.com/api',
   tokenName: 'bakensweetsToken',
-  siteKey: '6Lf8XUodAAAAAN_wbdoV37oas0iRYiRSBmSdCKJG',
+  siteKey: '6LcTo6AdAAAAABew0nDRAn1oACM5sa4Vwu_07qf0',
   pageSize: 12
 }
